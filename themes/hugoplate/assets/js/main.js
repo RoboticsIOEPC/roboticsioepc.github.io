0319ca281f994fbaeb7f887d90ff0a93e5390dfc
scientiac <thescientiac@protonmail.ch>
2023-12-10 09:47:03 +0545

    fixed dropdown toggle on mobile

--- themes/hugoplate/assets/js/main.js
+++ themes/hugoplate/assets/js/main.js
@@ -1,6 +1,18 @@
 (function () {
   "use strict";
 
+  // Dropdown Menu Toggler For Mobile
+  // ----------------------------------------
+  const dropdownMenuToggler = document.querySelectorAll(
+    ".nav-dropdown > .nav-link",
+  );
+
+  dropdownMenuToggler.forEach((toggler) => {
+    toggler?.addEventListener("click", (e) => {
+      e.target.parentElement.classList.toggle("active");
+    });
+  });
+
   let testimonialSlider;
 
   // Initialize the testimonial slider
